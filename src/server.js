d5047ffe806c505c971af7acb311f91831201a7a
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-04-26 23:52:18 -0300

    feat: routes, methods, headers and stateful application

--- src/server.js
+++ src/server.js
@@ -1,6 +1,39 @@
 import http from 'node:http'
 
+// GET - Buscar registros
+// POST - Criar um registro
+// PUT - Alterar um registro
+// PATCH - Alterar uma informação específica de um registro
+// DELETE - Remover um registro
+
+// stateful - dados armazenados localmente em memoria
+// stateless
+
+const users = []
+
+// JSON - JavaScript Object Notation
+
+// Cabeçalhos (Requisição/Resposta) => Metadados
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 
