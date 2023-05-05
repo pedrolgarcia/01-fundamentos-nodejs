a0a5e5975b918307e6181722052e42c6045a03ce
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-05-04 23:29:01 -0300

    feat: uuid

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+
 import { json } from './middlewares/json.js'
 import { Database } from './database.js'
 
@@ -32,7 +34,7 @@ const server = http.createServer(async (req, res) => {
     const { name, email } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       email,
     }
