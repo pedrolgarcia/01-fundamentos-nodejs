567a86b901131692b29916120daffec31f6feb46
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-05-05 00:10:45 -0300

    feat: build route parameters

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
 
 import { json } from './middlewares/json.js'
-import { Database } from './database.js'
+import { routes } from './routes.js'
 
 // GET - Buscar registros
 // POST - Criar um registro
@@ -13,35 +12,29 @@ import { Database } from './database.js'
 // stateful - dados armazenados localmente em memoria
 // stateless
 
-const database = new Database()
-
 // JSON - JavaScript Object Notation
 
 // Cabeçalhos (Requisição/Resposta) => Metadados
+
+// Query Parameters: URL Stateful => Filtros, paginação, não obrigatórios - ex: http://localhost:3333/users?userId=1&name=Pedro
+// Route Parameters: Identificação de recurso - ex: http://localhost:3333/users/1
+// Request Body: Envio de informações de um formulário (HTTPs) - ex: { "name": "Pedro", "email": "pedro@pedro.com" }
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find((route) => {
+    return route.method === method && route.path.test(url)
+  })
 
-    return res.end(JSON.stringify(users))
-  }
+  if (route) {
+    const routeParams = req.url.match(route.path)
 
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
+    req.params = { ...routeParams.groups }
 
-    const user = {
-      id: randomUUID(),
-      name,
-      email,
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
