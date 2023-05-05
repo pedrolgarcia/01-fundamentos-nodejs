de62d27345af4e94b786e81b7f53d9563b40659a
Pedro Lopes <pedrolopesgrc@gmail.com>
2023-05-04 22:58:36 -0300

    feat: middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 // GET - Buscar registros
 // POST - Criar um registro
@@ -18,24 +19,10 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  console.log(req)
-
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-type', 'application/json')
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
